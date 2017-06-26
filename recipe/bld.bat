bdcd549a99093a8e4c6246897abc74f65f46c5ea
Mathieu Carette <mathieu@forespell.com>
2017-06-26 17:13:54 +0200

    Win: skip python 3.6 and enable rtree

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ set PLATFORM=x64
 cl shell.c sqlite3.c -Fesqlite3.exe /DSQLITE_EXPORTS
 
 :: build the dll
-cl sqlite3.c -link -dll -out:sqlite3.dll
+cl sqlite3.c -link -dll -out:sqlite3.dll -DSQLITE_ENABLE_RTREE
 
 COPY sqlite3.exe %LIBRARY_BIN%\
 COPY sqlite3.dll %LIBRARY_BIN%\
